d22550414bb19c9b7122687be064cad6e59f6ea3
santiagoburgos <sburgos@itba.edu.ar>
2021-03-29 22:38:10 -0300

    webconfig javaMailSender

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,15 +1,16 @@
 package ar.edu.itba.paw.webapp.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @EnableWebMvc
 @ComponentScan({
@@ -17,6 +18,8 @@ import javax.sql.DataSource;
         "ar.edu.itba.paw.service",
         "ar.edu.itba.paw.persistence",
 })
+
+
 
 @Configuration
 public class WebConfig {
@@ -33,6 +36,7 @@ public class WebConfig {
 
   @Bean
   public DataSource dataSource(){
+
     final SimpleDriverDataSource ds = new SimpleDriverDataSource();
     ds.setDriverClass(org.postgresql.Driver.class);
     ds.setUrl("jdbc:postgresql://localhost/paw");
@@ -41,5 +45,24 @@ public class WebConfig {
 
     return ds;
   }
+
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+      JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+      mailSender.setHost("smtp.gmail.com");
+      mailSender.setPort(587);
+
+      mailSender.setUsername("recordpaw2020@gmail.com");
+      mailSender.setPassword("twytgaptuqsrpurs");
+
+      Properties props = mailSender.getJavaMailProperties();
+      props.put("mail.transport.protocol", "smtp");
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.debug", "true");
+
+         return mailSender;
+        }
+
 
 }
