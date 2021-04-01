608fc06850016d6d17a78300b15b7dc98538e81c
Manuel <mluque@itba.edu.ar>
2021-04-01 02:56:01 -0300

    Error fix in userdaoimpl

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -44,7 +45,14 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User register(final String username,final String password,final String first_name,final String last_name,final String email,final String phone) {
-        final Number userId = jdbcInsert.executeAndReturnKey(Map.of("username", username,"password",password,"first_name",first_name,"last_name",last_name,"email", email,"phone",phone));
+        MapSqlParameterSource params =new MapSqlParameterSource();
+        params.addValue("username", username);
+        params.addValue("password", password);
+        params.addValue("first_name", first_name);
+        params.addValue("last_name", last_name);
+        params.addValue("email", email);
+        params.addValue("phone_number", phone);
+        final Number userId = jdbcInsert.executeAndReturnKey(params);
         return new User(userId.longValue(), username,password, first_name,last_name,email,phone);
     }
 
