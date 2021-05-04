6813b429fbc88c369c53c5e1f3ac97c5e73b2143
Gian <gmarchetti@itba.edu.ar>
2021-05-04 11:55:36 -0300

    message properties and tag enum

---
+++ models/src/main/java/ar/edu/itba/paw/model/Tag.java
@@ -0,0 +1,13 @@
+public enum Tag {
+    AMERICAN, 
+    ARGENTINIAN, 
+    STEAKHOUSE, 
+    WITH_FRIENDS, 
+    ROMANTIC, 
+    INTERNATIONAL, 
+    PIZZERIA, 
+    ITALIAN, 
+    ASIAN, 
+    MEXICAN;
+
+}
