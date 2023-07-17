c61e4260898fa2a05a1c2279da9ff4c45bc650f1
Santiago Reyes <sreyes@itba.edu.ar>
2023-07-17 14:53:19 -0300

    added missing dependency

--- webapp/spa2/src/components/RestaurantForm/RestaurantForm.tsx
+++ webapp/spa2/src/components/RestaurantForm/RestaurantForm.tsx
@@ -43,7 +43,6 @@ export function RestaurantForm() {
     formState: { errors },
     setError,
     clearErrors,
-    getValues,
     setValue,
   } = useForm<IRestaurantRegister>({
     mode: "onTouched",
